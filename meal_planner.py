054caa4c7095cad56536bb37268aa77ccf401e82
yaren <yarencnk03@gmail.com>
2023-07-30 21:19:45 +0300

    just testing

--- meal_planner.py
+++ meal_planner.py
@@ -9,4 +9,20 @@ print("""
                             \|
 """)
  
- 
+class Recipes:
+    def __init__(self, type, name, diet):
+        self.type = type
+        self.name = name
+        self.diet = diet
+
+
+user_name = input("Welcome to your very own meal planner. What's your name? ")
+print(user_name + " please answer some quick questions for me to create a special meal plan for you.")
+
+print("How would you describe your eating habits?")
+user_eating_habit = input("""Your choices are:
+1. two meals a day(or IF)
+2. three meals a day
+3. three meals a day + a snack
+(Please enter the number of your choice.)""")
+
