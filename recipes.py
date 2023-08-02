6a12910798ddc82731642075836c9159bffdb6d6
yaren <140947345+ycnkc@users.noreply.github.com>
2023-08-02 22:25:04 +0300

    recipes

---
+++ recipes.py
@@ -0,0 +1,357 @@
+breakfast_recipes = {"Cottage Cheese Pancakes": 
+"""Cottage Cheese Pancakes(Single serving):
+100 g low-fat cottage Cheese
+60 g all purpose flour
+2 eggs
+1 tbsp sweetener of choice
+1 tsp baking powder
+1/2 tsp vanilla extract
+
+1. Add cottage cheese, eggs, monkfruit erythritol sweetener and vanilla extract in blender. 
+Blend until very smooth.
+2. Sift in flour and baking powder. Blend again until just combined. Do not overmix.
+3. Heat up a nonstick skillet over medium-low heat.
+Drop a ladleful of batter in the skillet and cook for 2 minutes on each side.
+4. Repeat until all batter is used up.
+5. Serve immediately with toppings of choice. Enjoy!""",
+
+"Carrot Cake Oatmeal":
+ """Carrot Cake Oatmeal:
+45g rolled oats
+45g carrot (half a small carrot)
+240ml water
+180ml non-fat milk
+1 egg white
+20g raisins
+1 tsp ground cinnamon
+1/4 tsp allspice
+1 tsp vanilla extract
+5-10 drops stevia
+1/4 tsp salt
+20g walnuts, chopped
+ 
+1. Grate carrot using box grater.
+2. Add grated carrot, water, cinnamon, allspice, vanilla extract and salt to a pot.
+Bring to a simmer over medium-low heat then cover and cook for 1 minute.
+3. Add rolled oats, egg white and raisins. Stir to combine. Cook over low heat for 2-3 minutes, stirring constantly.
+4. Add milk and cook for 1-2 minutes, stirring constantly.
+5. Sweeten to taste with stevia. Top with chopped walnuts and enjoy!""",
+ 
+ "Strawberry Cheesecake Overnight Oats":
+"""Strawberry Cheesecake Overnight Oats:
+45 g rolled oats
+120 ml low fat milk(or any non-dairy milk)
+1 tbsp chia seeds
+4 strawberries
+1 tbsp sweetener
+2 tbsp low-fat Greek yogurt(or any non-dairy yogurt)
+
+toppings:
+strawberry
+2tbsp low-fat Greek Yogurt
+1/2 crumbled biscuits
+
+1. Make strawberry compote - cut strawberries in halves and place in a microwave-safe bowl.
+Microwave on high for 1 minute, stir, then microwave for another 30 seconds. Let cool.
+2. Add strawberry compote and the rest of the overnight oats ingredients to a jar and stir to combine, making sure all the oats are submerged.
+3. Cover and refrigerate overnight or for at least 2 hours.
+4. The next morning, give everything a good stir and top with toppings. Enjoy!""",
+
+"Pizza Omelet":
+"""Pizza Omelet(2 servings):
+4 eggs
+2 tbsp tomato passata
+1/4 tsp dried oregano
+6 slices pepperoni
+30g shredded cheese
+2 black olives, sliced
+1/4 tsp olive oil
+Salt, to taste
+Pepper, to taste
+
+1. Mix tomato passata and dried oregano in a small bowl and set aside.
+2. Beat eggs in a bowl with a pinch of salt and pepper.
+3. Heat up olive oil in an oven-safe skillet over medium-low heat.
+4. Add beaten eggs and cook, covered, until eggs are set.
+5. Spread tomato sauce all over and top omelette with pepperoni, cheese and olives.
+6. Transfer omelette to oven and cook under grill setting under cheese is golden and bubbly.
+7. Cut into slices and serve immediately. Enjoy!""",
+
+"Apple Pie Granola":
+"""Apple Pie Granola:
+270g rolled oats
+100g pecans
+1 tbsp coconut oil, melted
+3 tbsp almond butter
+1 tsp maple syrup
+2 tsp cinnamon
+1/2 tsp sweetener
+1/2 tsp salt
+50g dried apple
+
+1. Preheat oven to 350°F / 180°C and line a baking tray with parchment paper.
+2. In a large mixing bowl, mix together rolled oats, pecans and 1.5 tsp cinnamon.
+3. In another mixing bowl, mix together melted coconut oil, almond butter, maple syrup, 
+monkfruit concentrate and salt until combined.
+4. Drizzle wet mixture over dry mixture and toss to combine.
+5. Transfer mixture to lined baking tray and bake for 10-15 minutes or until golden.
+6. Remove granola from the oven and let cool completely.
+9. Meanwhile, cut dried apple into 0.4 inch cubes using kitchen scissors.
+Then toss in the remaining 1/2 tsp cinnamon to coat evenly.
+10. When granola is completely cool, stir in cinnamon apple pieces.
+Store granola in an airtight container in a cool and dry place in the pantry. Enjoy!"""
+
+}
+
+main_dishes = {"Teriyaki Meatballs": 
+"""Teriyaki Meatballs(12 servings):
+500 g ground beef
+1 egg
+1 small onion
+1 medium carrot
+1 rib celery
+3-4 tbsp panko crumbs
+1/2 tsp garlic powder
+1/4 tsp ground ginger
+1/4 tsp black pepper
+1/4 tsp salt
+
+Teriyaki Glaze:
+120 ml mirin
+120 ml cooking sake
+120 ml light soy sauce
+100 g sweetener
+1 tsp cornstarch
+
+1. Preheat oven to 200°C / 400°F and prepare a nonstick baking tray.
+2. Put celery, carrot and onion in a food processor and process until very fine. Alternatively, you can just finely chop them using a knife.
+3. In a large mixing bowl, add all ingredients for the meatballs and mix well until combined. If the mixture looks too wet, add more panko crumbs, 1 tbsp at a time.
+4. Take 1 large spoonful of mixture and roll into a ball (around the size of a golf ball) using your hands. Transfer to baking tray. Repeat until all the mixture is used up.
+5. Bake meatballs for 15 minutes or until cooked through.
+6. While the meatballs are baking, make your glaze by combining all ingredients for the glaze in a microwave-safe bowl. Microwave for 2-3 minutes in 30 second intervals, stirring every pause, until the mixture thickens, and let it cool. It will continue to thicken as it cools down.
+7. Remove meatballs from oven. Pour teriyaki glaze over them and toss gently to coat evenly.
+8. Serve immediately with steamed rice or vegetables. Enjoy!
+""",
+"Gochujang Mac and Cheese": 
+"""Gochujang Mac and Cheese:
+240ml low fat milk
+50g sharp cheddar cheese
+4 tsp all purpose flour
+1 tbsp butter
+2 tsp gochujang (Korean red chili paste)
+2 cloves garlic
+Salt, to taste
+250g chickpea pasta
+
+1. Chop garlic and grate cheddar cheese.
+2. Cook pasta according to package instructions. Reserve about 120ml pasta water and drain. Set aside.
+3. In a large saucepan, add butter, garlic and gochujang. Cook over medium-low heat until fragrant.
+4. Whisk in flour and about 60ml of milk until smooth. Then, stir in remaining milk and bring to a boil, whisking constantly, until thickened.
+5. Remove from heat and stir in grated cheese. Season to taste with salt.
+6. Add cooked pasta and stir to coat pasta in sauce. Thin out the sauce with pasta water, 1 tbsp at a time, if necessary.
+7. Serve immediately in your favourite pasta bowl.
+8. Alternatively, you can bake the mac and cheese. Transfer to an oven safe dish and top with panko crumbs.
+9. Lightly spray with cooking spray, then broil in the oven for 2-3 minutes or until golden brown. Enjoy!
+""",
+"Chicken Lo Mein": 
+"""Chicken Lo Mein:
+250g chicken breast
+250g drained shirataki noodles
+1 bunch choy sum
+1 carrot
+1 scallion
+1 tsp chicken bouillon powder
+1/4 tsp cooking oil
+1 tsp toasted sesame seeds
+Sauce:
+1/2 tbsp soy sauce
+1/2 tbsp hoisin sauce
+1/2 tsp dark soy sauce
+1/2 tsp cornstarch
+1/2 tsp sesame oil
+1/2 tsp monkfruit erythritol sweetener
+1/4 tsp garlic powder
+1/4 tsp white pepper
+
+Prepare the ingredients:
+1. Roughly chop choy sum, separating the leafy parts from the stems. Roughly chop scallion, cut carrot into matchsticks, and cut chicken into thin strips.
+2. In a bowl, mix chicken with chicken bouillon powder until combined. Set aside.
+3. Prepare the shirataki noodles according to package instructions. Alternatively, follow the instructions here.
+4. In a bowl, make the sauce by mixing all the sauce ingredients until combined. Set aside.
+Cook:
+1. Heat up cooking oil in a cast iron pan or a large nonstick skillet over medium-high heat.
+2. Add scallion and chicken and stir-fry until chicken changes color, about 3-4 minutes. Remove from pan and set aside.
+3. Add carrot and choy sum stems. Cook until tender.
+4. Add choy sum leaves, cooked chicken, shirataki noodles and sauce to the pan.
+5. Stir-fry until everything is well coated in the sauce and the sauce is thickened slightly.
+6. Remove from heat. Garnish with toasted sesame seeds. Enjoy!
+""",
+"Chicken Fried Rice":
+"""
+Chicken Fried Rice:
+200g chicken breast
+2 eggs
+250g cooked rice
+1 broccoli
+1 carrot
+1/2 tsp toasted sesame seeds (for garnish)
+1/2 tsp shaoxing wine
+1 tsp light soy sauce
+1/2 tsp oil
+Sauce:
+1 tbsp light soy sauce
+1 tbsp sriracha
+1 tsp dark soy sauce
+1/2 tsp monkfruit erythritol sweetener
+1/4 tsp white pepper
+1/4 tsp garlic powder
+
+1 Dice broccoli, carrot and chicken breast.
+2. Marinate chicken in shaoxing wine and light soy sauce for 10 minutes.
+3. Make the sauce by mixing all the sauce ingredients in a small bowl until combined. Set aside.
+4. Beat the eggs in a small bowl. Set aside.
+5. Cook the fried rice
+6. Heat up the oil in a large nonstick skillet over medium heat. Add chicken and cook until it changes color. Remove from heat and set aside.
+7. Add diced broccoli and carrot to skillet and cook until tender, stirring constantly. Add a splash of water as needed to prevent burning or sticking.
+8. Add beaten eggs and scramble until set.
+9. Add cooked rice and chicken and stir-fry for 2 minutes.
+10.Increase the heat to high, add sauce and toss until everything is coated in the sauce evenly. Stir-fry for another minute then remove from heat.
+Garnish with toasted sesame seeds and serve immediately. Enjoy!
+""",
+"Spaghetti Puttanesca":
+"""Spaghetti Puttanesca:
+1 tbsp olive oil
+3 cloves garlic
+1/4 tsp chili flakes
+400g canned tomatoes
+6 anchovy fillets, drained and oil reserved
+80g black olives
+80g capers
+300g dry spaghetti
+Pepper, to taste
+Optional toppings:
+40g panko crumbs
+1 tbsp oil from anchovies
+Lemon zest
+Freshly grated parmesan
+
+1. Toast panko crumbs (if using)
+2. Heat up anchovy oil in a nonstick skillet over medium heat.
+3. Add panko crumbs and stir to coat evenly. Cook, stirring often, until panko is golden brown, about 2 minutes.
+4. Remove from heat and let cool.
+Make pasta:
+1. Mince garlic. Chop anchovies, olives and capers.
+2. Heat up 1 tbsp olive oil in a large nonstick skillet over medium heat. Add garlic and anchovies and cook for 3 minutes.
+3. Add chopped olives and capers and stir to combine.
+4. Add canned tomatoes and stir to combine. Let mixture gently simmer over low heat.
+5. Meanwhile, cook spaghetti according to package instructions until al dente.
+6. Reserve 1 cup of cooking water and drain spaghetti.
+7. Add drained spaghetti and 2-3 tbsp of cooking water to the simmering tomato sauce, stirring to combine.
+8. Cook for 1-2 extra minutes. Add more cooking water as needed if it is too thick.
+9. Season to taste with pepper.
+10. Serve immediately. Top with toasted panko crumbs, freshly grated lemon zest and parmesan. Enjoy!
+""",
+"Microwave Baked Potato":
+"""Microwave Baked Potato:
+2 russet potatoes
+1/2 tsp olive oil (optional)
+1/2 tsp coarse sea salt (optional)
+Toppings:
+Light sour cream/ Non-fat Greek yogurt
+Chopped parsley
+Shredded cheddar
+Grilled corn kernels
+Hot sauce
+Salt & Pepper
+
+1. Prick each potato a few times on each side with a fork.
+2. Place potatoes in the microwave, directly on the turntable.
+Microwave for 3-4 minutes on each side. They should be tender and easily poked through with a fork.
+3. Carefully remove potatoes from microwave and wrap in a clean kitchen towel. Let sit for 2 minutes before opening.
+4. Slice potatoes in halves and top as desired. Enjoy!
+Optional: if you want the potatoes to have crispy skin, rub olive oil and sea salt over the cooked potatoes, 
+then bake in a preheated 200°C / 400°F toaster oven or oven for 10 minutes or until crispy.
+"""}
+
+snacks = {"Peanut Butter Choc Chip Protein Balls":
+"""Peanut Butter Choc Chip Protein Balls:
+80g rolled oats
+125g natural peanut butter
+9 tbsp ground flaxseed
+3 tbsp maple syrup
+30g sugar-free chocolate chips
+1/2 tsp vanilla extract
+
+1. In a large mixing bowl, add rolled oats and ground flaxseed and mix until combined.
+2. Add peanut butter and maple syrup to a microwave-safe bowl. 
+Microwave on high for 30 seconds or until peanut butter has melted.
+Add mixture to rolled oats along with vanilla extract. Mix very well until combined.
+3. Mix in chocolate chips.
+4. Using your hands, shape mixture into small balls.
+5. Enjoy right away or store in an airtight container in the fridge.""",
+"Healthy Chocolate Cupcakes":
+"""Healthy Chocolate Cupcakes:
+cupcakes:
+200 g sweet potato puree
+83 g apple sauce
+240 ml milk of choice
+2 tbsp coconut oil, melted
+2 tbsp apple cider vinegar
+1/2 tsp vanilla extract
+200 g all purpose flour
+5 tbsp dutch cocoa powder
+1 tsp baking soda
+1/4 tsp salt
+frosting:
+200 g sweet potato puree
+130 g semi-sweet chocolate chips
+pinch of salt
+
+Bake the cupcakes:
+1. Preheat oven to 180°C / 350°F and line a muffin tin with paper liners.
+2. In a large mixing bowl, combine sweet potato puree, apple sauce, milk,
+melted coconut oil, apple cider vinegar, stevia and vanilla extract.
+3. In another mixing bowl, combine flour, cocoa powder, baking soda and salt.
+4. Add wet mixture into dry mixture and mix until just combined. Do not overmix.
+5. Transfer batter to lined muffin tin. 
+Bake for 20 minutes or until an inserted stick comes out clean.
+6. Let cupcakes cool completely before frosting.
+Make the frosting
+1. While waiting for cupcakes to bake and cool, make the frosting.
+2. Heat up sweet potato puree until warm and transfer to a food processor.
+The sweet potato puree should be warm enough to melt the chocolate chips.
+3. Add chocolate chips and salt. Process until chocolate is melted and fully combined with the sweet potato puree.
+4. Let frosting cool then transfer to a piping bag then frost and decorate your cupcakes as desired.
+""",
+"Cinnamon Raisin Protein Balls":
+"""Cinnamon Raisin Protein Balls:
+80 g rolled oats
+80 g almond butter
+30 g plant-based vanilla protein powder
+3 tbsp sugar-free maple flavored syrup
+1/2 tsp cinnamon powder
+1/2 tsp vanilla extract
+2 tbsp raisins
+pinch of salt
+
+Place all ingredients in a large mixing bowl and mix until thoroughly combined.
+Scoop out a spoonful of mixture and roll into a ball. Repeat until all the mixture is used up.
+Store in an airtight container in the fridge.
+""",
+"Pizza Zucchini Bites":
+"""Pizza Zucchini Bites:
+1 large zucchini
+50 g pizza sauce
+80 g shredded mozzarella cheese
+toppings(optional):
+diced green bell pepper
+diced pitted olives
+mini pepperoni
+shredded basil
+
+1. Preheat oven to 200°C / 400°F.
+2. Slice zucchini into 0.5 inch slices.
+3. Arrange zucchini slices on a nonstick baking sheet in a single layer. 
+Top each slice with pizza sauce, shredded cheese and desired toppings.
+4. Bake for 8 minutes or until cheese is golden and bubbly.
+5. Remove from oven and serve immediately. Enjoy!"""}
